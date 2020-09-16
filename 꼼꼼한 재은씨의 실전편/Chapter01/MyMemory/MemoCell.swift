aeefcd30c34f59b2d23ca147023036f011b8e01e
csh8130 <csh8130@naver.com>
2020-09-17 02:30:19 +0900

    1.1 프로젝트 생성 및 메모장 인터페이스 설계 까지

---
+++ 꼼꼼한 재은씨의 실전편/Chapter01/MyMemory/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  MyMemory
+//
+//  Created by Choi SeungHyuk on 2020/09/17.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
