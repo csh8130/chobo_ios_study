ec0a4352b575381298898dc4ab61ec56ce7e02b8
csh8130 <csh8130@naver.com>
2020-09-20 20:11:01 +0900

    @State 대신 @ObservedObject를 사용

--- pointfree.co/Composable Architecture/01. SwiftUI and State Management/Part 1/MyPlayground.playground/Contents.swift
+++ pointfree.co/Composable Architecture/01. SwiftUI and State Management/Part 1/MyPlayground.playground/Contents.swift
@@ -1,48 +1,64 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    NavigationView {
-      List {
-        NavigationLink(destination: CounterView()) {
-          Text("Counter demo")
+    @ObservedObject var state: AppState
+    var body: some View {
+        NavigationView {
+            List {
+                NavigationLink(destination: CounterView(state: self.state)) {
+                    Text("Counter demo")
+                }
+                NavigationLink(destination: EmptyView()) {
+                    Text("Favorite primes")
+                }
+            }
+            .navigationBarTitle("State management")
         }
-        NavigationLink(destination: EmptyView()) {
-          Text("Favorite primes")
-        }
-      }
-        .navigationBarTitle("State management")
     }
-  }
+}
+
+class AppState: ObservableObject {
+    var count: Int = 0 {
+        willSet {
+            self.objectWillChange.send()
+        }
+    }
 }
 
 struct CounterView: View {
-  var body: some View {
-    VStack {
-      HStack {
-        Button(action: {}) {
-          Text("-")
+    @ObservedObject var state: AppState
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: { self.state.count -= 1 }) {
+                  Text("-")
+                }
+                Text("\(self.state.count)")
+                Button(action: { self.state.count += 1 }) {
+                  Text("+")
+                }
+            }
+            Button(action: {}) {
+                Text("Is this prime?")
+            }
+            Button(action: {}) {
+                Text("What's the \(ordinal(self.state.count)) prime?")
+            }
         }
-        Text("0")
-        Button(action: {}) {
-          Text("+")
-        }
-      }
-      Button(action: {}) {
-        Text("Is this prime?")
-      }
-      Button(action: {}) {
-        Text("What is the 0th prime?")
-      }
+        .font(.title)
+        .navigationBarTitle("Counter demo")
     }
-    .font(.title)
-    .navigationBarTitle("Counter demo")
-  }
+    
+    private func ordinal(_ n: Int) -> String {
+      let formatter = NumberFormatter()
+      formatter.numberStyle = .ordinal
+      return formatter.string(for: n) ?? ""
+    }
 }
 
 import PlaygroundSupport
 
 PlaygroundPage.current.liveView = UIHostingController(
-  rootView: ContentView()
+    rootView: ContentView(state: AppState())
 //  rootView: CounterView()
 )
