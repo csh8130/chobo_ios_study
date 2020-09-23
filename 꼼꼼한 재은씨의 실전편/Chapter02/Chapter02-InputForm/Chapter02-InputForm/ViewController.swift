b428ef5bdac7a87dd3a194d27626a19a0d30db30
csh8130 <csh8130@naver.com>
2020-09-23 23:51:29 +0900

    챕터2

--- 꼼꼼한 재은씨의 실전편/Chapter02/Chapter02-InputForm/Chapter02-InputForm/ViewController.swift
+++ 꼼꼼한 재은씨의 실전편/Chapter02/Chapter02-InputForm/Chapter02-InputForm/ViewController.swift
@@ -11,7 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     
     var paramEmail: UITextField!
-
+    var paramUpdate: UISwitch!
+    var paramInterval: UIStepper!
+    var txtUpdate: UILabel!
+    var txtInterval: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -44,10 +48,76 @@ class ViewController: UIViewController {
         self.paramEmail.frame = CGRect(x: 120, y: 100, width: 220, height: 30)
         self.paramEmail.font = UIFont.systemFont(ofSize: 13)
         self.paramEmail.borderStyle = .roundedRect
+        self.paramEmail.autocapitalizationType = .none
         
         self.view.addSubview(paramEmail)
+        
+        self.paramUpdate = UISwitch()
+        self.paramUpdate.frame = CGRect(x: 120, y: 150, width: 50, height: 30)
+        
+        self.paramUpdate.setOn(true, animated: true)
+        
+        self.view.addSubview(self.paramUpdate)
+        
+        self.paramInterval = UIStepper()
+        
+        self.paramInterval.frame = CGRect(x: 120, y: 200, width: 50, height: 30)
+        self.paramInterval.minimumValue = 0
+        self.paramInterval.maximumValue = 100
+        self.paramInterval.stepValue = 1
+        self.paramInterval.value = 0
+        
+        self.view.addSubview(self.paramInterval)
+        
+        self.txtUpdate = UILabel()
+        
+        self.txtUpdate.frame = CGRect(x: 250, y: 150, width: 100, height: 30)
+        self.txtUpdate.font = UIFont.systemFont(ofSize: 12)
+        self.txtUpdate.textColor = UIColor.red
+        self.txtUpdate.text = "갱신함"
+        
+        self.view.addSubview(self.txtUpdate)
+        
+        self.txtInterval = UILabel()
+        self.txtInterval.frame = CGRect(x: 250, y: 200, width: 100, height: 30)
+        self.txtInterval.font = UIFont.systemFont(ofSize: 12)
+        self.txtInterval.textColor = UIColor.red
+        self.txtInterval.text = "0분마다"
+        
+        self.view.addSubview(self.txtInterval)
+        
+        self.paramUpdate.addTarget(self, action: #selector(presentUpdateValue(_:)), for: .valueChanged)
+        self.paramInterval.addTarget(self, action: #selector(presentIntervalValue(_:)), for: .valueChanged)
+        
+        let submitBtn = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(submit(_:)))
+        self.navigationItem.rightBarButtonItem = submitBtn
+        
+//        for family in UIFont.familyNames {
+//            print("\(family)")
+//            for names in UIFont.fontNames(forFamilyName: family) {
+//                print("== \(names)")
+//            }
+//        }
+        
+//        let customFont = UIFont(name: "NotoSansMyanmar-Regular", size: 12)
+//        lblEmail.font = customFont
     }
 
-
+    @objc func presentUpdateValue(_ sender: UISwitch) {
+        self.txtUpdate.text = (sender.isOn == true ? "갱신함" : "갱신하지 않음")
+    }
+    
+    @objc func presentIntervalValue(_ sender: UIStepper) {
+        self.txtInterval.text = ("\(Int(sender.value))분마다")
+    }
+    
+    @objc func submit(_ sender: Any) {
+        let rvc = ReadViewController()
+        rvc.pEmail = self.paramEmail.text
+        rvc.pUpdate = self.paramUpdate.isOn
+        rvc.pInterval = self.paramInterval.value
+        
+        self.navigationController?.pushViewController(rvc, animated: true)
+    }
 }
 
