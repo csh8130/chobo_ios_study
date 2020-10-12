0b9466992e8ce7885edc6d6a21c4bfcfaa79727a
csh8130 <csh8130@naver.com>
2020-10-13 00:14:03 +0900

    IBOutlet, IBAction 연결

--- 꼼꼼한 재은씨의 실전편/Chapter05/Chapter05-CustomPlist/Chapter05-CustomPlist/ListViewController.swift
+++ 꼼꼼한 재은씨의 실전편/Chapter05/Chapter05-CustomPlist/Chapter05-CustomPlist/ListViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ListViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    var accountlist = ["asdasd@aSdasd","Asdasd@asdsad","afSDF@sdfdsf","dsfsfgrw@egwf"]
+    var accountlist = [String]()
     
     @IBOutlet weak var account: UITextField!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var gender: UISegmentedControl!
+    @IBOutlet weak var married: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +41,12 @@ class ListViewController: UITableViewController, UIPickerViewDelegate, UIPickerV
         new.action = #selector(newAccount(_:))
         
         toolbar.setItems([new, flexSpace, done], animated: true)
+        
+        let plist = UserDefaults.standard
+        
+        self.name.text = plist.string(forKey: "name")
+        self.married.isOn = plist.bool(forKey: "married")
+        self.gender.selectedSegmentIndex = plist.integer(forKey: "gender")
     }
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -57,10 +66,59 @@ class ListViewController: UITableViewController, UIPickerViewDelegate, UIPickerV
         self.account.text = account
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 1 {
+            let alert = UIAlertController(title: nil, message: "이름을 입력하세요", preferredStyle: .alert)
+            alert.addTextField {
+                $0.text = self.name.text
+            }
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+                let value = alert.textFields?[0].text
+                let plist = UserDefaults.standard
+                plist.setValue(value, forKey: "name")
+                plist.synchronize()
+                self.name.text = value
+            }))
+            self.present(alert, animated: false, completion: nil)
+        }
+    }
+    
     @objc func pickerDone(_ sender: Any) {
         self.view.endEditing(true)
     }
     
     @objc func newAccount(_ sender: Any) {
+        self.view.endEditing(true)
+        let alert = UIAlertController(title: "새 계정을 입력하세요", message: nil, preferredStyle: .alert)
+        alert.addTextField {
+            $0.placeholder = "ex) abc@gmail.com"
+        }
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            if let account = alert.textFields?[0].text {
+                self.accountlist.append(account)
+                self.account.text = account
+                
+                self.name.text = ""
+                self.gender.selectedSegmentIndex = 0
+                self.married.isOn = false
+            }
+        }))
+        self.present(alert, animated: false, completion: nil)
     }
+    
+    @IBAction func changeGender(_ sender: UISegmentedControl) {
+        let value = sender.selectedSegmentIndex
+        let plist = UserDefaults.standard
+        plist.set(value, forKey: "gender")
+        plist.synchronize()
+    }
+    
+    @IBAction func changeMarried(_ sender: UISwitch) {
+        let value = sender.isOn
+        let plist = UserDefaults.standard
+        plist.set(value, forKey: "married")
+        plist.synchronize()
+    }
+
 }
