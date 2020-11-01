056ef0d4ec75d0d123983da5239a2292d44c5db3
csh8130 <csh8130@naver.com>
2020-11-02 01:21:12 +0900

    boilerplate를 줄이는 typealias

--- pointfree.co/Composable Architecture/03. Modularity/Part 01/project/PrimeModal/PrimeModal.swift
+++ pointfree.co/Composable Architecture/03. Modularity/Part 01/project/PrimeModal/PrimeModal.swift
@@ -7,19 +7,11 @@
 
 import Foundation
 
+public typealias PrimeModalState = (count: Int, favoritePrimes: [Int])
+
 public enum PrimeModalAction {
     case saveFavoritePrimeTapped
     case removeFavoritePrimeTapped
-}
-
-public struct PrimeModalState {
-  public var count: Int
-  public var favoritePrimes: [Int]
-
-  public init(count: Int, favoritePrimes: [Int]) {
-    self.count = count
-    self.favoritePrimes = favoritePrimes
-  }
 }
 
 public func primeModalReducer(state: inout PrimeModalState, action: PrimeModalAction) -> Void {
