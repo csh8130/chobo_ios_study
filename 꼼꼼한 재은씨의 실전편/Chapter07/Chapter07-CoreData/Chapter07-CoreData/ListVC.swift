06bb353318fb4870fbf54d86f9431d27cf10e240
csh8130 <csh8130@naver.com>
2020-11-03 01:40:18 +0900

    chapter7 init

---
+++ 꼼꼼한 재은씨의 실전편/Chapter07/Chapter07-CoreData/Chapter07-CoreData/ListVC.swift
@@ -0,0 +1,14 @@
+//
+//  ListVC.swift
+//  Chapter07-CoreData
+//
+//  Created by Choi SeungHyuk on 2020/11/03.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ListVC: UITableViewController {
+    
+}
