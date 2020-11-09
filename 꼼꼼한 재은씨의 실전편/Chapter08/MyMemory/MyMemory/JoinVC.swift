b5022916b30cd512b650e259b74fa71b79b3488b
csh8130 <csh8130@naver.com>
2020-11-09 23:47:42 +0900

    아이디 생성 뷰 컨트롤러 추가

---
+++ 꼼꼼한 재은씨의 실전편/Chapter08/MyMemory/MyMemory/JoinVC.swift
@@ -0,0 +1,23 @@
+//
+//  JoinVC.swift
+//  MyMemory
+//
+//  Created by Choi SeungHyuk on 2020/11/09.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class JoinVC: UIViewController {
+    @IBOutlet weak var profile: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func submit(_ sender: Any) {
+    }
+}
