bde83fc31633feafc80c4e80c0462c0859f74af8
csh8130 <csh8130@naver.com>
2020-12-06 19:02:47 +0900

    Side Effects init

---
+++ pointfree.co/Composable Architecture/04. Side Effects/Part 01/project/ComposableArchiteture/ComposableArchiteture.swift
@@ -0,0 +1,69 @@
+import SwiftUI
+import Combine
+
+public final class Store<Value, Action>: ObservableObject {
+    @Published public private(set) var value: Value
+    private var cancellable: Cancellable?
+    
+    let reducer: (inout Value, Action) -> Void
+    public init(initialValue: Value, reducer: @escaping (inout Value, Action) -> Void) {
+      self.value = initialValue
+      self.reducer = reducer
+    }
+    
+    public func send(_ action: Action) {
+      self.reducer(&self.value, action)
+    }
+    
+    public func view<LocalValue, LocalAction>(
+        value toLocalValue: @escaping (Value) -> LocalValue,
+        action toGlobalAction: @escaping (LocalAction) -> Action
+    ) -> Store<LocalValue, LocalAction> {
+        let localStore = Store<LocalValue, LocalAction>(
+            initialValue: toLocalValue(self.value),
+            reducer: { localValue, localAction in
+                self.send(toGlobalAction(localAction))
+                localValue = toLocalValue(self.value)
+            }
+        )
+        localStore.cancellable = self.$value.sink { [weak localStore] newValue in
+            localStore?.value = toLocalValue(newValue)
+        }
+        return localStore
+    }
+}
+
+public func combine<Value, Action>(
+  _ reducers: (inout Value, Action) -> Void...
+) -> (inout Value, Action) -> Void {
+
+  return { value, action in
+    for reducer in reducers {
+      reducer(&value, action)
+    }
+  }
+}
+
+public func pullback<GlobalValue, LocalValue, GlobalAction, LocalAction>(
+  _ reducer: @escaping (inout LocalValue, LocalAction) -> Void,
+  value: WritableKeyPath<GlobalValue, LocalValue>,
+  action: WritableKeyPath<GlobalAction, LocalAction?>
+) -> (inout GlobalValue, GlobalAction) -> Void {
+
+  return { globalValue, globalAction in
+    guard let localAction = globalAction[keyPath: action] else { return }
+    reducer(&globalValue[keyPath: value], localAction)
+  }
+}
+
+public func logging<Value, Action>(
+  _ reducer: @escaping (inout Value, Action) -> Void
+) -> (inout Value, Action) -> Void {
+  return { value, action in
+    reducer(&value, action)
+    print("Action: \(action)")
+    print("State:")
+    dump(value)
+    print("---")
+  }
+}
---
+++ pointfree.co/Composable Architecture/04. Side Effects/Part 01/project/project/ContentView.swift
@@ -0,0 +1,147 @@
+//
+//  ContentView.swift
+//  project
+//
+//  Created by Choi SeungHyuk on 2020/11/02.
+//
+
+import SwiftUI
+import ComposableArchiteture
+import FavoritePrimes
+import Counter
+//import PrimeModal
+
+struct ContentView: View {
+    @ObservedObject var store: Store<AppState, AppAction>
+    var body: some View {
+        NavigationView {
+            List {
+                NavigationLink(
+                    "Counter demo",
+                    destination: CounterView(
+                        store: self.store.view(
+                            value: { $0.counterView },
+                            action: { .counterView($0) }
+                        )
+                    )
+                )
+                NavigationLink(
+                  "Favorite primes",
+                  destination: FavoritePrimesView(
+                    store: self.store.view { $0.favoritePrimes } action: { .favoritePrimes($0) }
+                  )
+                )
+            }
+            .navigationBarTitle("State management")
+        }
+    }
+}
+
+struct AppState {
+  var count = 0
+  var favoritePrimes: [Int] = []
+  var activityFeed: [Activity] = []
+  var loggedInUser: User? = nil
+
+  struct Activity {
+    let type: ActivityType
+    let timestamp = Date()
+
+    enum ActivityType {
+      case addedFavoritePrime(Int)
+      case removedFavoritePrime(Int)
+    }
+  }
+
+  struct User {
+    let id: Int
+    let name: String
+    let bio: String
+  }
+}
+
+enum AppAction {
+//  case counter(CounterAction)
+//  case primeModal(PrimeModalAction)
+    case counterView(CounterViewAction)
+    case favoritePrimes(FavoritePrimesAction)
+
+    var favoritePrimes: FavoritePrimesAction? {
+        get {
+            guard case let .favoritePrimes(value) = self else { return nil }
+            return value
+        }
+        set {
+            guard case .favoritePrimes = self, let newValue = newValue else { return }
+            self = .favoritePrimes(newValue)
+        }
+    }
+    
+    var counterView: CounterViewAction? {
+      get {
+        guard case let .counterView(value) = self else { return nil }
+        return value
+      }
+      set {
+        guard case .counterView = self, let newValue = newValue else { return }
+        self = .counterView(newValue)
+      }
+    }
+}
+
+func activityFeed(
+  _ reducer: @escaping (inout AppState, AppAction) -> Void
+) -> (inout AppState, AppAction) -> Void {
+
+  return { state, action in
+    switch action {
+    case .counterView(.counter):
+      break
+
+    case .counterView(.primeModal(.removeFavoritePrimeTapped)):
+      state.activityFeed.append(
+        .init(type: .removedFavoritePrime(state.count))
+      )
+
+    case .counterView(.primeModal(.saveFavoritePrimeTapped)):
+      state.activityFeed.append(
+        .init(type: .addedFavoritePrime(state.count))
+      )
+
+    case let .favoritePrimes(.deleteFavoritePrimes(indexSet)):
+      for index in indexSet {
+        state.activityFeed.append(
+          .init(type: .removedFavoritePrime(state.favoritePrimes[index]))
+        )
+      }
+    }
+    reducer(&state, action)
+  }
+}
+
+struct _KeyPath<Root, Value> {
+  let get: (Root) -> Value
+  let set: (inout Root, Value) -> Void
+}
+
+extension AppState {
+    var counterView: CounterViewState {
+        get {
+            CounterViewState(
+                count: self.count,
+                favoritePrimes: self.favoritePrimes
+            )
+        }
+        set {
+            self.count = newValue.count
+            self.favoritePrimes = newValue.favoritePrimes
+        }
+    }
+}
+
+let _appReducer: (inout AppState, AppAction) -> Void = combine(
+    pullback(counterViewReducer, value: \.counterView, action: \.counterView),
+    pullback(favoritePrimesReducer, value: \.favoritePrimes, action: \.favoritePrimes)
+)
+
+let appReducer = pullback(_appReducer, value: \.self, action: \.self)
