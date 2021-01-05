9bf804b0c9a4d5f846e223e2f27905cdfb3d4ae9
csh8130 <csh8130@naver.com>
2021-01-05 16:39:35 +0900

    navbar tint 그라데이션, 시계영역 흰색으로 변경

--- 캘린더 커스텀 연습/accountBook/accountBook/ViewController.swift
+++ 캘린더 커스텀 연습/accountBook/accountBook/ViewController.swift
@@ -11,9 +11,39 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setNavGradient()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    
+    func setNavGradient() {
+        if let navigationBar = self.navigationController?.navigationBar {
+            let gradient = CAGradientLayer()
+            var bounds = navigationBar.bounds
+            bounds.size.height += UIApplication.shared.statusBarFrame.size.height
+            gradient.frame = bounds
+            gradient.colors = [UIColor.init(red: 115/256, green: 196/256, blue: 177/256, alpha: 1).cgColor, UIColor.init(red: 114/256, green: 186/256, blue: 211/256, alpha: 1).cgColor]
+            gradient.startPoint = CGPoint(x: 0, y: 0)
+            gradient.endPoint = CGPoint(x: 1, y: 0)
+            
+            if let image = getImageFrom(gradientLayer: gradient) {
+                navigationBar.setBackgroundImage(image, for: UIBarMetrics.default)
+            }
+        }
     }
 
 
+    func getImageFrom(gradientLayer:CAGradientLayer) -> UIImage? {
+        var gradientImage:UIImage?
+        UIGraphicsBeginImageContext(gradientLayer.frame.size)
+        if let context = UIGraphicsGetCurrentContext() {
+            gradientLayer.render(in: context)
+            gradientImage = UIGraphicsGetImageFromCurrentImageContext()?.resizableImage(withCapInsets: UIEdgeInsets.zero, resizingMode: .stretch)
+        }
+        UIGraphicsEndImageContext()
+        return gradientImage
+    }
 }
-
