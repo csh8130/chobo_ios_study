1751c656535f25aa2503a335286aeceea87f0e04
csh8130 <csh8130@naver.com>
2021-01-12 17:47:37 +0900

    캘린더 월마다 week 갯수 마다 뷰 크기 조절 애니메이션 추가 (단 첫번째 주가 일요일부터 시작하는 경우 오류 남아있음)

--- 캘린더 커스텀 연습/accountBook/accountBook/ViewController.swift
+++ 캘린더 커스텀 연습/accountBook/accountBook/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import FSCalendar
 
 class ViewController: UIViewController {
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var bottomViewTop: NSLayoutConstraint!
     @IBOutlet weak var calendar: FSCalendar!
     
     override func viewDidLoad() {
@@ -57,5 +59,80 @@ class ViewController: UIViewController {
 extension ViewController: FSCalendarDelegate, FSCalendarDataSource {
     func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
         setTitleMonth(calendar)
+        let lastDay = lastDayInMonth(calendar.currentPage)
+        let weekCount = nthWeek(lastDay)
+        let height = CGFloat(6 - weekCount) * calendar.visibleCells()[0].fs_height
+
+        UIView.animate(withDuration: 0.3,
+                       delay: 0,
+                       options: [.curveEaseInOut],
+                       animations: { [weak self] () -> Void in
+                        guard let self = self else { return }
+                        self.bottomViewTop.constant = height
+                        self.view.layoutIfNeeded()
+                       }, completion: { [weak self] _ in
+                       })
+    }
+    
+    func nthWeek(_ d: Date) -> Int {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "W"
+        let nth = formatter.string(from: d)
+        return Int(nth) ?? -1
+    }
+    
+    func lastDayInMonth(_ date: Date) -> Date {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.locale = Locale(identifier: "ko")
+
+        //여기에 기입하지 않은 날짜는 1로 초기화가 된다
+        let components = calendar.dateComponents([.year, .month], from: date)
+
+        //day를 기입하지 않아서 현재 달의 첫번쨰 날짜가 나오게 된다
+        let startOfMonth = calendar.date(from: components)!
+        let comp1 = calendar.dateComponents([.day,.weekday,.weekOfMonth], from: startOfMonth)
+
+        //이번 달의 마지막 날짜를 구해주기 위해서 다음달을 구한다 이것도 day를 넣어주지 않았기 때문에 1이 다음달의 1일이 나오게 된다
+        let nextMonth = calendar.date(byAdding: .month, value: +1, to: startOfMonth)
+
+        //위에 값에서 day값을 넣어주지 않았기 떄문에 1이 나오게 되므로 마지막 날을 알기 위해서 -1을 해준다
+        let endOfMonth = calendar.date(byAdding: .day, value: -1, to:nextMonth!)
+        
+        return endOfMonth!
+    }
+    
+    func test(_ date: Date) { //그 달의 첫째날, 마지막날 구해서 그 사이를 순회
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.locale = Locale(identifier: "ko")
+
+        //여기에 기입하지 않은 날짜는 1로 초기화가 된다
+        let components = calendar.dateComponents([.year, .month], from: date)
+
+        //day를 기입하지 않아서 현재 달의 첫번쨰 날짜가 나오게 된다
+        let startOfMonth = calendar.date(from: components)!
+        let comp1 = calendar.dateComponents([.day,.weekday,.weekOfMonth], from: startOfMonth)
+
+        //이번 달의 마지막 날짜를 구해주기 위해서 다음달을 구한다 이것도 day를 넣어주지 않았기 때문에 1이 다음달의 1일이 나오게 된다
+        let nextMonth = calendar.date(byAdding: .month, value: +1, to: startOfMonth)
+
+        //위에 값에서 day값을 넣어주지 않았기 떄문에 1이 나오게 되므로 마지막 날을 알기 위해서 -1을 해준다
+        let endOfMonth = calendar.date(byAdding: .day, value: -1, to:nextMonth!)
+                
+        let comp2 = calendar.dateComponents([.day,.weekday,.weekOfMonth], from: endOfMonth!)
+        let weekArr = calendar.shortWeekdaySymbols
+
+        if let weekday = comp1.weekday, let weekday2 = comp2.weekday {
+            print(weekArr[weekday-1])
+            print(weekArr[weekday2-1])
+        }
+        //이 달의 첫번째 날과 끝날을 구했으니깐 그 사이의 날짜는 for문을 통해서 구하면 된다
+        //현재 만들어 놓은 달력의 label변수들을 배열에 저장하여서 for문을 돌면서 차례대로 값을 집어 넣으면 되는데
+        //label값에 entry값을 집어 넣는 위치는 첫번째 날짜의 weekday를 기준으로 하면 된다
+        for i in comp1.day!...comp2.day! {
+            print(i)
+        }
     }
 }
+
+
