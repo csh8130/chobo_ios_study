cb199b5852f2ec9111456844328423ce7c86665f
csh8130 <csh8130@naver.com>
2021-01-12 18:21:30 +0900

    디테일(금액 입력) 뷰 추가

---
+++ 캘린더 커스텀 연습/accountBook/accountBook/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  accountBook
+//
+//  Created by Choi SeungHyuk on 2021/01/12.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var dayTitle: UILabel!
+    @IBOutlet weak var input: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func pay(_ sender: Any) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
