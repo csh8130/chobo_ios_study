1f8a52239e72a2915a16163f4243bfbc5c1f94aa
csh8130 <csh8130@naver.com>
2021-03-06 12:52:57 +0900

    tmp

--- iOS AutoLayout 완벽 가이드 - 실무 프로젝트를 위한 실전강의/Section2/Section2/Section2/ViewController.swift
+++ iOS AutoLayout 완벽 가이드 - 실무 프로젝트를 위한 실전강의/Section2/Section2/Section2/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var btn1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultLabel.layer.cornerRadius = 50
+//        resultLabel.layer.masksToBounds = true
+        resultLabel.clipsToBounds = true
+        print(btn1.bounds.width)
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        print(btn1.bounds.width)
+        btn1.layer.cornerRadius = btn1.bounds.width / 2
+    }
 }
 
