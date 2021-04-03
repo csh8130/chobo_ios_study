157f4ef82bd3279f964761c0278a3dba0a7f7741
csh8130 <csh8130@naver.com>
2021-04-04 02:43:50 +0900

    [instagram firestore 강의] 2. 메인 탭의 viewControllers에 VC 추가

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -8,9 +8,25 @@
 import UIKit
 
 class MainTabController: UITabBarController {
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureViewController() {
+        view.backgroundColor = .white
+
+        let feed = FeedController()
+        let search = SearchController()
+        let imageSelector = ImageSelectorController()
+        let notifications = NotificationController()
+        let profile = ProfileController()
         
-        view.backgroundColor = .red
+        viewControllers = [feed, search, imageSelector, notifications, profile]
     }
 }
