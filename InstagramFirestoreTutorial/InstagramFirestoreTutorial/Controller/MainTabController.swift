ede227c57d61dc7f94c19cd171e3b82be19b7d35
csh8130 <csh8130@naver.com>
2021-04-03 16:47:27 +0900

    [instagram firestore 강의] 1. MainTabControlelr 추가 후 첫번째 화면으로 변경

---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Choi SeungHyuk on 2021/04/03.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
