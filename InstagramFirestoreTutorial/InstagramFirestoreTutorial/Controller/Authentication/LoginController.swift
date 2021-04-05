6e6dfa4a272ad4c29efe46df3cbdb146109eae8f
csh8130 <csh8130@naver.com>
2021-04-06 00:40:11 +0900

    [instagram firestore 강의] 7. LoginViewController 추가

---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Choi SeungHyuk on 2021/04/06.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK : - Properties
+    
+    // MARK : - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,21 @@
+//
+//  RegistrationController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Choi SeungHyuk on 2021/04/06.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK : - Properties
+    
+    // MARK : - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
