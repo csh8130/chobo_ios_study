6d9771175c50a33ffeae55c8aadc1c22036b292d
csh8130 <csh8130@naver.com>
2021-04-06 00:32:50 +0900

    [instagram firestore 강의] 6. feed cell 포스팅 이미지 추가

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/FeedCell.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/FeedCell.swift
@@ -29,6 +29,59 @@ class FeedCell: UICollectionViewCell {
         return button
     }()
     
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.isUserInteractionEnabled = true
+        iv.backgroundColor = .purple
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        return iv
+    }()
+    
+    private lazy var likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "like_unselected"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var commentButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "comment"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var shareButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "send2"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private let likesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1 like"
+        label.font = .boldSystemFont(ofSize: 12)
+        return label
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "captionLabel"
+        label.font = .boldSystemFont(ofSize: 12)
+        return label
+    }()
+    
+    private let postTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2 days ago"
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .lightGray
+        return label
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -43,6 +96,10 @@ class FeedCell: UICollectionViewCell {
         
         addSubview(usernameButton)
         usernameButton.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
+        
+        addSubview(postImageView)
+        postImageView.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 8)
+        postImageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true
     }
     
     required init?(coder: NSCoder) {
