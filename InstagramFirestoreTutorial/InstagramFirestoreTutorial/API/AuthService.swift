ce2c82304785644bd90f447d937e2461a92c6d3f
csh8130 <csh8130@naver.com>
2021-04-11 01:55:35 +0900

    [instagram firestore 강의] 14. 회원 가입 API stub 생성 및 버튼 연결

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/API/AuthService.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/API/AuthService.swift
@@ -5,4 +5,19 @@
 //  Created by Choi SeungHyuk on 2021/04/11.
 //
 
-import Foundation
+import UIKit
+import Firebase
+
+struct AuthCredentials {
+    let email: String
+    let password: String
+    let fullname: String
+    let username: String
+    let profileImage: UIImage
+}
+
+struct AuthService {
+    static func registerUser(withCredential credentials: AuthCredentials) {
+        print("DEBUG: Credentials are \(credentials)")
+    }
+}
