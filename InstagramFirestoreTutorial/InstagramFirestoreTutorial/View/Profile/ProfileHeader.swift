59e317265e2f2713eb3a53cd82615762912a661d
csh8130 <csh8130@naver.com>
2021-04-21 01:14:28 +0900

    [instagram firestore 강의] 18. name Label, edit Profile Button 추가

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/Profile/ProfileHeader.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/Profile/ProfileHeader.swift
@@ -19,6 +19,25 @@ class ProfileHeader: UICollectionReusableView {
         return iv
     }()
     
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Eddie Brock"
+        label.font = .boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    private lazy var editProfileFollowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Edit Profile", for: .normal)
+        button.layer.cornerRadius = 3
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 0.5
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(handleEditProfileFollowTapped), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -30,9 +49,22 @@ class ProfileHeader: UICollectionReusableView {
         profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 16, paddingLeft: 12)
         profileImageView.setDimensions(height: 80, width: 80)
         profileImageView.layer.cornerRadius = 80 / 2
+        
+        addSubview(nameLabel)
+        nameLabel.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
+        
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.anchor(top: nameLabel.bottomAnchor, left: leftAnchor,
+                         right: rightAnchor, paddingTop: 16,
+                         paddingLeft: 24, paddingRight: 24)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Actions
+    
+    @objc func handleEditProfileFollowTapped() {
     }
 }
