033fdd44b2dd72495e584ba7be97ba7fdcdee7e5
csh8130 <csh8130@naver.com>
2021-04-21 01:01:07 +0900

    [instagram firestore 강의] 18. 프로필 이미지, 포스트 이미지 추가

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/Profile/ProfileHeader.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/Profile/ProfileHeader.swift
@@ -11,12 +11,25 @@ class ProfileHeader: UICollectionReusableView {
     
     // MARK: - Properties
     
+    private let profileImageView: UIImageView = {
+       let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .systemPink
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 16, paddingLeft: 12)
+        profileImageView.setDimensions(height: 80, width: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
     }
     
     required init?(coder: NSCoder) {
