7cf7ad317b4f17dd795bb7037e161b1b168fb39d
csh8130 <csh8130@naver.com>
2021-04-21 17:52:38 +0900

    [instagram firestore 강의] 21. 프로필 컨트롤러 사용자 정의 init 추가 (의존성 주입)
    프로필 컨트롤러의 User는 nil인 케이스가 있을 수 없도록 변경, 그외에 어떤 api 호출도 없어짐
    api호출은 더 상위인 MainTabController에서 수행

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -12,13 +12,27 @@ class MainTabController: UITabBarController {
     
     // MARK: - Lifecycle
     
+    private var user: User? {
+        didSet {
+            guard let user = user else { return }
+            configureViewController(withUser: user)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureViewController()
         checkIfUserIsLoggedIn()
+        fetchUser()
     }
     
     // MARK: - API
+    
+    func fetchUser() {
+        UserService.fetchUser { user in
+            self.user = user
+        }
+    }
+    
     func checkIfUserIsLoggedIn() {
         // https://firebase.google.com/docs/auth/ios/manage-users?hl=ko
         if Auth.auth().currentUser == nil {
@@ -34,7 +48,7 @@ class MainTabController: UITabBarController {
     
     // MARK: - Helpers
     
-    func configureViewController() {
+    func configureViewController(withUser user: User) {
         view.backgroundColor = .white
 
         let layout = UICollectionViewFlowLayout()
@@ -43,8 +57,8 @@ class MainTabController: UITabBarController {
         let imageSelector = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectorController())
         let notifications = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationController())
         
-        let profileLayout = UICollectionViewFlowLayout()
-        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController(collectionViewLayout: profileLayout))
+        let profileController = ProfileController(user: user)
+        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: profileController)
         
         viewControllers = [feed, search, imageSelector, notifications, profile]
         
@@ -62,7 +76,7 @@ class MainTabController: UITabBarController {
 
 extension MainTabController: AuthenticationDelegate {
     func authenticationDidComplete() {
-        print("DEBUG: Auth did complete. fetch user and update here")
+        fetchUser()
         self.dismiss(animated: true, completion: nil)
     }
 }
--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/ProfileController.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/ProfileController.swift
@@ -14,32 +14,30 @@ class ProfileController: UICollectionViewController {
     
     // MARK: - Properties
     
-    var user: User? {
-        didSet {
-            collectionView.reloadData()
-        }
-    }
+    private var user: User
     
     // MARK: - Lifecycle
+    
+    init(user: User) {
+        self.user = user
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
-        fetchUser()
     }
     
     // MARK: - API
-    
-    func fetchUser() {
-        UserService.fetchUser { user in
-            self.user = user
-            self.navigationItem.title = user.username
-        }
-    }
-    
+
     // MARK: - Helpers
     
     func configureCollectionView() {
+        navigationItem.title = user.username
         collectionView.backgroundColor = .white
         collectionView.register(ProfileCell.self, forCellWithReuseIdentifier: cellIdentifier)
         collectionView.register(ProfileHeader.self,
@@ -69,9 +67,7 @@ extension ProfileController {
             return UICollectionReusableView()
         }
         
-        if let user = user {
-            header.viewModel = ProfileHeaderViewModel(user: user)
-        }
+        header.viewModel = ProfileHeaderViewModel(user: user)
         
         return header
     }
