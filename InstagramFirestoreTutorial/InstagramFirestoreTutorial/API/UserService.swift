6691a7f2baceed1522a31a0105f326b4ef69c12a
csh8130 <csh8130@naver.com>
2021-04-21 13:24:02 +0900

    [instagram firestore 강의] 19. Constant 추가

---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/API/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Choi SeungHyuk on 2021/04/21.
+//
+
+import Foundation
---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Utils/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Choi SeungHyuk on 2021/04/21.
+//
+
+import Firebase
+
+let COLLECTION_USERS = Firestore.Firestore().collection("users")
