f05fe0cd19017a2e0a218555c7e9ed948184cf9a
csh8130 <csh8130@naver.com>
2021-05-26 00:27:28 +0900

    [instagram firestore 강의] 41. 코멘트 셀 > input accesory view 생성

---
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/CommentInputAccesoryView.swift
@@ -0,0 +1,58 @@
+//
+//  CommentInputAccesoryView.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Choi SeungHyuk on 2021/05/26.
+//
+
+import UIKit
+
+class CommentInputAccesoryView: UIView {
+    
+    // MARK: - Properties
+    
+    private let commentTextView: InputTextView = {
+        let tv = InputTextView()
+        tv.placeholderText = "Enter comment.."
+        tv.font = .systemFont(ofSize: 15)
+        tv.isScrollEnabled = false
+        return tv
+    }()
+    
+    private let postButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Post", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(handlePostTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(postButton)
+        postButton.anchor(top: topAnchor, right: rightAnchor, paddingRight: 8)
+        postButton.setDimensions(height: 50, width: 50)
+        
+        addSubview(commentTextView)
+        commentTextView.anchor(top: topAnchor, left: leftAnchor, bottom: safeAreaLayoutGuide.bottomAnchor, right: postButton.leftAnchor, paddingTop: 8, paddingLeft: 8, paddingBottom: 8, paddingRight: 8)
+        
+        let divider = UIView()
+        divider.backgroundColor = .lightGray
+        addSubview(divider)
+        divider.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor, height: 0.5)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Actions
+    
+    @objc func handlePostTapped() {
+        
+    }
+}
